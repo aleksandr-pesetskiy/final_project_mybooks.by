a717ed919c5c11d16efc7ea5f28b00c29771b083
Alexandr <alexandr.pesetskiy@gmail.com>
2025-07-19 17:14:40 +0300

    add log4g

--- src/main/java/by/mybooks/ui/BasePage.java
+++ src/main/java/by/mybooks/ui/BasePage.java
@@ -1,6 +1,8 @@
 package by.mybooks.ui;
 
 import by.mybooks.ui.expectedMessages.ExpectedMessages;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import by.mybooks.ui.driver.Driver;
@@ -8,31 +10,34 @@ import by.mybooks.ui.pages.authorizationForm.AuthorizationFormLocator;
 
 public class BasePage {
 
-    protected WebDriver driver;
+    protected final WebDriver driver;
+    protected final Logger logger = LogManager.getLogger(getClass());
 
     public BasePage() {
         driver = Driver.getDriver();
+        logger.info("Инициализация BasePage");
     }
 
     public void open() {
+        logger.info("Открытие главной страницы: {}", ExpectedMessages.HOME_PAGE);
         driver.get(ExpectedMessages.HOME_PAGE);
         try {
             clickButtonAcceptCookie();
+            logger.info("Cookie закрыты");
             clickButtonCabinet();
-            getAuthorizationForm();
-        } catch (Exception ignored) {
+            logger.info("Открыта форма авторизации");
+        } catch (Exception e) {
+            logger.warn("Не удалось принять Cookie: {}", e.getMessage());
         }
     }
 
     public void clickButtonAcceptCookie() {
+        logger.info("Принятие Cookie");
         driver.findElement(By.xpath(AuthorizationFormLocator.BUTTON_ACCEPT_COOKIE)).click();
     }
 
     public void clickButtonCabinet() {
+        logger.info("Открытие формы авторизации");
         driver.findElement(By.xpath(AuthorizationFormLocator.BUTTON_CABINET)).click();
-    }
-
-    public void getAuthorizationForm() {
-        driver.findElement(By.xpath(AuthorizationFormLocator.WINDOW_MODAL_AUTHORIZATION));
     }
 }
